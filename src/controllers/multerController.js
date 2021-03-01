22e851c0939455e999999a823d262daa2a9a559c
Andres Sierra <dante.andres.swei@gmail.com>
2021-02-28 22:03:59 -0500

    Test

--- src/controllers/multerController.js
+++ src/controllers/multerController.js
@@ -13,11 +13,11 @@ const storage = multer.diskStorage({
 module.exports = multer({ 
         storage:storage,
         fileFilter:function(req, file, cb){
-            if(file.mimetype === 'text/csv'){
+            /*if(file.mimetype === 'text/csv'){
                 cb(null,true);
             }else{
                 cb(null, false);
                 return cb(new Error('Solo se permiten archivos .CSV'));
-            }
+            }*/
         }
     }).single('file');   
